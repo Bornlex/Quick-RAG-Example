08072668ba43aa19cf5dc6c9fa07d468ea92b36d
Julien <jseveno.piltant@gmail.com>
2024-03-25 23:32:42 +0100

    Log requests

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+
+
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'ping': client.ping()})
